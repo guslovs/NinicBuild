2923411b7e027990ac7c302bb693a76f491620e7
guslovs <guslovs609@gmail.com>
2026-02-22 15:26:54 +0100

    HomePage link update

--- src/components/HomePage/Section4.jsx
+++ src/components/HomePage/Section4.jsx
@@ -1,12 +1,13 @@
 import photo from '../../assets/team.png'
 import styles from './Section2.module.css'
+import { Link } from 'react-router-dom'
 
 export default function Section4() {
     return (
         <div className={styles.container}>
             <div className={styles.leftDiv}>
                 <h1>Naša Posvećenost Razvoju</h1>
-                <p>Stalno unapređujemo svoje veštine i način rada kako bismo pružili još bolju uslugu, a više o tome možete pronaći u sekciji <a href="/about">‘O nama’</a>.</p>
+                <p>Stalno unapređujemo svoje veštine i način rada kako bismo pružili još bolju uslugu, a više o tome možete pronaći u sekciji <Link to="/about">‘O nama’</Link>.</p>
             </div>
             <div className={styles.rightDiv}>
                 <img src={photo} alt="Plans" />
